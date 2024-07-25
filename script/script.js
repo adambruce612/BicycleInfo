6dbeb245178ff79cd26c29841f29f2ad59a23666
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2024-07-25 17:32:01 -0500

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,1 +1,22 @@
-console.log("test");
+const bicycleInfo = document.getElementById("bicycle-info");
+
+function bicycleSelection()
+{
+    let bicycle = document.getElementById("bikes").value;
+    displayInfo(bicycle);
+}
+
+const trek = 
+{
+    name: "Trek 7.2fx",
+    type: "Hybrid",
+    color: "Black",
+    frame: "FX Alpha Silver Aluminum, DuoTrap S compatible, rack & fender mounts",
+    fork: "High-tensile steel w/lowrider mounts, CLIX dropouts"
+}
+
+
+function displayInfo()
+{
+    bicycleInfo.innerHTML = `<p>${trek.name}</p>`
+}
