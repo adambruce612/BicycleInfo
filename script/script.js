aa998b13d3fceb0163461deffa900425e8133733
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2024-07-27 18:55:05 -0500

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,5 +1,9 @@
 const bicycleInfo = document.getElementById("bicycle-info");
 const bikeSelect = document.getElementById("bike-select");
+const trek = document.getElementById("trek");
+
+console.log(trek);
+
 
 bikeSelect.addEventListener("change", bikeSelection)
 
@@ -9,31 +13,12 @@ function bikeSelection(event)
     let bicycleSelected;
 
     if (bicycle == "trek") {
-        bicycleSelected = trek;
+        trek.className = "visible";
     } else if (bicycle == "stumpjumper") {
         bicycleSelected = stumpjumper;
     }
-
-    displayInfo(bicycleSelected);
 }
 
-function displayInfo(bicycleSelected)
-{
-    bicycleInfo.innerHTML = `<p>${bicycleSelected.name}</p>`;
-}
 
-const trek = {
-    name: "Trek 7.2fx",
-    type: "Hybrid",
-    color: "Black",
-    frame: "FX Alpha Silver Aluminum, DuoTrap S compatible, rack & fender mounts",
-    fork: "High-tensile steel w/lowrider mounts, CLIX dropouts"
-}
 
-const stumpjumper = 
-{
-    name: "Specialized S-Works Stumpjumper",
-    type: "Mountain Bike",
-    color: "Black",
-    manual: "https://media.specialized.com/support/collateral/Specialized_Owners_Manual_World.pdf"
-}
+
