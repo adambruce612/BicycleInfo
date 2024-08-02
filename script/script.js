405acd4ba4cbb191456871cea74a1b6b39c69067
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2024-08-02 16:02:53 -0500

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -2,21 +2,34 @@ const bicycleInfo = document.getElementById("bicycle-info");
 const bikeSelect = document.getElementById("bike-select");
 const trek = document.getElementById("trek");
 const stumpjumper = document.getElementById("stumpjumper");
+const fatboy = document.getElementById("fatboy");
 
-let activeSelection;
+bikeSelect.addEventListener("change", bikeSelection);
 
-bikeSelect.addEventListener("change", bikeSelection)
+let activeSelection = null;
 
 function bikeSelection(event)
 {
     let bicycle = event.target.value;
-    let bicycleSelected;
+
+    if (activeSelection != null)
+    {
+        activeSelection.classList.add("hidden");
+    }
 
     if (bicycle == "trek") {
         trek.classList.remove("hidden");
+        activeSelection = trek;
     } else if (bicycle == "stumpjumper") {
         stumpjumper.classList.remove("hidden");
+        activeSelection = stumpjumper;
+    } else if (bicycle == "fatboy")
+    {
+        fatboy.classList.remove("hidden");
+        activeSelection = fatboy;
     }
+
+    console.log(activeSelection);
 }
 
 
